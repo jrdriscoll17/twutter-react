886704ddf1ff81d3d8398a3286e4ca1295ae1852
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-11-25 22:11:05 -0600

    Adds index and show to users controller

--- twutter-react-backend/app/controllers/users_controller.rb
+++ twutter-react-backend/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
 end
