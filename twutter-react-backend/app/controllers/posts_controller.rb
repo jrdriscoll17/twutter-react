d3c367c972531e47488f4501fa18bd2f5a78c233
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-11-25 22:08:42 -0600

    Adds create, show, index, and destroy for posts controller

--- twutter-react-backend/app/controllers/posts_controller.rb
+++ twutter-react-backend/app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = 1
+
+    if post.save
+      render json: post
+    else
+      render json: post.errors.messages
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
