e43c402deef7c8740b4dc527e4fb5edfb1701f74
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-11-30 16:03:15 -0600

    Changes delete argument name to make it clearer what was being passed in

--- twutter-react-frontend/src/actions/deletePost.js
+++ twutter-react-frontend/src/actions/deletePost.js
@@ -1,11 +1,11 @@
-export default function deletePost(post) {
+export default function deletePost(postID) {
   return (dispatch) => {
-    fetch(`http://localhost:3000/posts/${post}`, {
+    fetch(`http://localhost:3000/posts/${postID}`, {
       method: 'DELETE',
     })
       .then((r) => r.json())
       .then((res) => {
-        dispatch({ type: 'DELETE_POST', payload: post });
+        dispatch({ type: 'DELETE_POST', payload: postID });
       });
   };
 }
