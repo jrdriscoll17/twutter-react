980c09a35f6cf717da04bf1d5c607d3160cf4167
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-11-30 15:20:18 -0600

    Refactored post form by removing edit and redirect

--- twutter-react-frontend/src/components/PostForm.js
+++ twutter-react-frontend/src/components/PostForm.js
@@ -4,46 +4,36 @@ import { connect } from 'react-redux';
 import addPost from '../actions/addPost';
 
 class PostForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: this.props.post ? this.props.post.name : '',
-      id: this.props.post ? this.props.post.id : '',
-    };
-  }
+  state = {
+    content: '',
+  };
 
   handleSubmit(event) {
-    debugger;
     event.preventDefault();
     this.props.addPost(this.state);
 
-    this.setState({ name: '', id: '' });
+    this.setState({ content: '' });
     this.props.history.push('/posts');
   }
 
   handleChange(event) {
-    let name = event.target.name;
     this.setState({
-      [name]: event.target.value,
+      content: event.target.value,
     });
   }
 
-  redirectOrRenderForm() {
+  render() {
     return (
       <form onSubmit={(event) => this.handleSubmit(event)}>
         <input
           type='text'
           onChange={(event) => this.handleChange(event)}
-          value={this.state.name}
-          name='name'
+          value={this.state.content}
+          name='content'
         />
         <input type='submit' />
       </form>
     );
-  }
-
-  render() {
-    return <>{this.redirectOrRenderForm()}</>;
   }
 }
 
